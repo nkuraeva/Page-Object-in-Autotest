fc4b1e6c21b9d9b4c54a5f94bbf0a40f609e2066
nkuraeva <kuraeva@gmail.com>
2021-02-02 12:41:12 +0100

    add class login_guest in test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -13,19 +13,20 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page.basket_should_be_empty()
     basket_page.text_basket_is_empty_should_be_present()
 
-@pytest.mark.skip
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-@pytest.mark.skip
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(sef, browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-#pytest -v --tb=line --language=en test_main_page.py
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+#pytest -v -m login_guest test_main_page.py
